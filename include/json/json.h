a76e55b53766c3c10e580ab9174666a7713636a5
Benny Bach <benny.bach@gmail.com>
2014-08-30 20:50:55 +0200

    Squashed 'lib/json/' content from commit 80ee324
    git-subtree-dir: lib/json
    git-subtree-split: 80ee324994cdacb16c62f4e3bc579894ca8b472f

---
+++ include/json/json.h
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------
+//
+//     Filename   : json.h
+//
+//     Author     : Benny Bach <benny.bach@gmail.com>
+//                  Copyright (C) 2014
+//
+// --- Description: -----------------------------------------------------------
+//
+//
+// ----------------------------------------------------------------------------
+#ifndef __json__json_h__
+#define __json__json_h__
+
+// ----------------------------------------------------------------------------
+#include <json/json_value.h>
+#include <json/json_array.h>
+#include <json/json_object.h>
+#include <json/json_parser.h>
+#include <json/json_error.h>
+
+// ----------------------------------------------------------------------------
+#endif // __json__json_h__

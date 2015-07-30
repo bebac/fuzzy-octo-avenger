f0d30f0fc346d2086e223fae4dbf016eeb253c5d
Benny Bach <benny.bach@gmail.com>
2015-07-30 11:27:42 +0200

    Webclient - Hideable sidebar menu

--- webclient/sinatra/app/web/views/application_view.rb
+++ webclient/sinatra/app/web/views/application_view.rb
@@ -22,6 +22,7 @@ class ApplicationView < Vienna::View
     @subview = create_view(name)
     @subview.render
     element.find('#view-container').css( :top => "#{top}px" )
+    element.find('#menu').css( :top => "#{top}px" )
     element.find('#view-container').html = @subview.element
     set_selected(name)
   end
@@ -45,6 +46,19 @@ class ApplicationView < Vienna::View
 
   def top
     Document.find('#nav-container').height
+  end
+
+  on :click, '#menu-toggle' do |evt|
+    menu = element.find('#menu')
+    cont = Document.find('#view-container')
+    menu.toggle(150, :swing, -> {
+      if menu.visible?
+        cont.css( :"width" => "#{cont.width-menu.width}px", :"left" => "#{menu.width}px" )
+      else
+        cont.css( :"width" => "100%", :"left" => "0" )
+      end
+    })
+    evt.stop_propagation
   end
 
 end
